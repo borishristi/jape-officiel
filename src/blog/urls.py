ea6257dff9c3db79daae0411fd76df8bc34d2383
Hristi <borisarroga@gmail.com>
2024-02-26 14:47:37 +0100

    I add the static files configurations

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from .views import base_view
+
+from jape_officiel import settings
+from .views import base_view, image_view
 
 urlpatterns = [
     path('', base_view, name="base_view"),
-]
+    path('1', image_view, name="image_view"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/blog/views.py
+++ src/blog/views.py
@@ -7,3 +7,7 @@ from django.shortcuts import render
 def base_view(request):
     return render(request, "base.html")
     # return HttpResponse("<h1>Ok c'est bon!!!</h1>")
+
+
+def image_view(request):
+    return render(request, "blog/article_01.html")
