0de39c31bca5e5fc49856e2f68f1abef84992cfb
Hristi <borisarroga@gmail.com>
2024-02-26 15:30:25 +0100

    I implement the blogpost mÃodel and add it in the administration panel

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+from django.utils.text import slugify
+
+User = get_user_model()
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255, unique=True, verbose_name='Titre')
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    last_update = models.DateTimeField(auto_now=True)
+    created_on = models.DateField(blank=True, null=True)
+    published = models.BooleanField(default=False, verbose_name="Publié")
+    image = models.ImageField(upload_to="images")
+    content = models.TextField(blank=True, verbose_name='Contenu')
+
+    class Meta:
+        ordering = ['-created_on']
+        verbose_name = "Article"
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+    @property
+    def author_or_default(self):
+        return self.author.username if self.author else "auteur inconnu"
+
+    # def get_absolute_url(self):
+    #     return reverse('posts:home')
