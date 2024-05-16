c14b6cab775385ecef07a4b1a48ce4f7152b9261
Hristi <borisarroga@gmail.com>
2024-05-16 12:08:33 +0200

    I implement the send_email function with gmail but it doesn't work

--- src/jape_officiel/settings.py
+++ src/jape_officiel/settings.py
@@ -129,7 +129,7 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # Send local mail
 # EMAIL_HOST = 'localhost'
@@ -142,5 +142,5 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = '587'
 EMAIL_HOST_USER = "japeofficiel@gmail.com"
-EMAIL_HOST_PASSWORD = "jape@2024"
+EMAIL_HOST_PASSWORD = "woxp rqhv psiq hsf"
 EMAIL_USE_TLS = True
